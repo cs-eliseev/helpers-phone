031b81eb122c55fb1949300eee84f87e1bcef8df
cs-eliseev <a.k.eliseev@gmail.com>
2019-03-01 13:16:46 +0300

    add file autoload

---
+++ autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+switch (true) {
+    case file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php'):
+        require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+        break;
+    case file_exists(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php'):
+        require_once __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'autoload.php';
+        break;
+    default:
+        $path = __DIR__ . DIRECTORY_SEPARATOR . 'src';
+
+        $iterator = new DirectoryIterator($path);
+
+        while ($iterator->valid()) {
+            $item = $iterator->current();
+
+            if($item->isFile()) {
+                require_once $path . DIRECTORY_SEPARATOR . $item->getFilename();
+            }
+
+            unset($item);
+            $iterator->next();
+        }
+
+        break;
+}
