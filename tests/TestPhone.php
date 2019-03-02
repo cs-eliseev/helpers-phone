c9f7a79373f658254118491c74faa633d5984386
cs-eliseev <a.k.eliseev@gmail.com>
2019-03-02 14:45:16 +0300

    add unit test format phone number

--- tests/TestPhone.php
+++ tests/TestPhone.php
@@ -76,4 +76,133 @@ class TestPhone extends TestCase
             ],
         ];
     }
+
+
+    /**
+     * @param $phone
+     * @param string $format
+     * @param bool $isMask
+     * @param string $pattern
+     * @param array $mask
+     * @param string $expected
+     *
+     * @dataProvider providerFormat
+     */
+    public function testFormat($phone, string $format, bool $isMask, string $pattern, array $mask, string $expected): void
+    {
+        $this->assertEquals($expected, Phone::format($phone, $format, $isMask, $pattern, $mask));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerFormat(): array
+    {
+        return [
+            [
+                4455,
+                Phone::FORMAT_DEFAULT,
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '44-55'
+            ],
+            [
+                3344455,
+                Phone::FORMAT_DEFAULT,
+                false,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '3344455'
+            ],
+            [
+                '+1 222 33 444 55',
+                Phone::FORMAT_DEFAULT,
+                true,
+                '(.{2})(.{3})(.{2})(.{3})(.*)',
+                Phone::REVERT_MASK,
+                '+1 (222) 33-444-55'
+            ],
+            [
+                '(222) 333-4455',
+                Phone::FORMAT_DEFAULT,
+                false,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '+ (222) 333-44-55'
+            ],
+            [
+                '((222) 333-44-55',
+                Phone::FORMAT_DEFAULT,
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '(222) 333-44-55'
+            ],
+            [
+                12223334455,
+                '$3-$4-$5',
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '333-44-55'
+            ],
+            [
+                '222 333-4455',
+                '$2',
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '222'
+            ],
+            [
+                '222-333-44-55',
+                '$1',
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                ''
+            ],
+            [
+                '(222)3334455',
+                Phone::FORMAT_DEFAULT,
+                true,
+                '(.{2})(.{2})(.{2})(.{4})(.*)',
+                Phone::REVERT_MASK,
+                '(2223) 33-44-55'
+            ],
+            [
+                '+122233344-55',
+                '$1 $2 $3 $4 $5 $6',
+                true,
+                '(.{2})(.{2})(.{2})(.{2})(.{2})(.*)',
+                ['$6' => '1$', '$5' => '2$', '$4' => '3$', '$3' => '4$', '$2' => '5$', '$1' => '6$'],
+                '1 22 23 33 44 55'
+            ],
+            [
+                '1-2223334455',
+                '$2 $3 $4 $5 $6',
+                true,
+                '(.{2})(.{2})(.{2})(.{2})(.{2})(.*)',
+                ['$6' => '1$', '$5' => '2$', '$4' => '3$', '$3' => '4$', '$2' => '5$', '$1' => '6$'],
+                '22 23 33 44 55'
+            ],
+            [
+                '222-333-4455   -Hello!',
+                Phone::FORMAT_DEFAULT,
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '(222) 333-44-55'
+            ],
+            [
+                '+1 - 2223334455',
+                '$1 ($2) $3 $4$5',
+                true,
+                Phone::PATTERN,
+                Phone::REVERT_MASK,
+                '1 (222) 333 4455'
+            ],
+        ];
+    }
 }
