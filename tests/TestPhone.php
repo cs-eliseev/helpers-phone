a3d06437ccd0eb024503b84b965d468bb636a9a6
cs-eliseev <a.k.eliseev@gmail.com>
2019-03-02 20:02:33 +0300

    add unit test exist phone number

--- tests/TestPhone.php
+++ tests/TestPhone.php
@@ -344,4 +344,75 @@ class TestPhone extends TestCase
             ],
         ];
     }
+
+
+    /**
+     * @param $phone
+     * @param int $sizeMin
+     * @param int $sizeMax
+     * @param $expected
+     *
+     * @dataProvider providerExist
+     */
+    public function testExist($phone, int $sizeMin, int $sizeMax, $expected): void
+    {
+        $this->assertEquals($expected, Phone::exist($phone, $sizeMin, $sizeMax));
+    }
+
+    /**
+     * @return array
+     */
+    public function providerExist(): array
+    {
+        return [
+            [
+                4455,
+                Phone::SIZE_MIN,
+                Phone::SIZE_MAX,
+                false
+            ],
+            [
+                3344455,
+                Phone::SIZE_MIN,
+                Phone::SIZE_MAX,
+                true
+            ],
+            [
+                3344455,
+                Phone::SIZE_MAX,
+                Phone::SIZE_MAX,
+                false
+            ],
+            [
+                '+1 222 33 444 55',
+                Phone::SIZE_MIN,
+                Phone::SIZE_MAX,
+                true
+            ],
+            [
+                '(222) 333-4455',
+                Phone::SIZE_MIN,
+                Phone::SIZE_MIN,
+                false
+            ],
+            [
+                '((222) 333-44-55',
+                Phone::SIZE_MIN,
+                Phone::SIZE_MAX,
+                true
+            ],
+            [
+                '222-333-4455   -Hello!',
+                Phone::SIZE_MIN,
+                Phone::SIZE_MAX,
+                true
+            ],
+            [
+                '+1 - 2223334455 Hello 67',
+                Phone::SIZE_MIN,
+                11,
+                false
+            ],
+        ];
+    }
 }
